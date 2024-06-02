afcca6598a9b729196fd416c1437b035948b38a7
Felipe Ribeiro <felipe.eduribeiro@gmail.com>
2024-06-01 23:32:56 -0300

    V2.1

--- website/server.js
+++ website/server.js
@@ -9,17 +9,20 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const redis = require('redis');
+const redisAdapter = require('socket.io-redis');
+
+// Configuração do Redis
+const pubClient = redis.createClient({ host: 'redis', port: 6379 });
+const subClient = redis.createClient({ host: 'redis', port: 6379 });
+io.adapter(redisAdapter({ pubClient, subClient }));
+
 const db = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE || 'lista_compras'
 });
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // Servindo arquivos estáticos da pasta public
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 io.on('connection', (socket) => {
     console.log('New client connected');
@@ -28,13 +31,18 @@ io.on('connection', (socket) => {
     });
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
     const sortBy = req.query.sortBy || 'item'; // Default sort by 'item' (alphabetical)
-    const sortOrder = req.query.sortOrder || 'DESC'; // Default sort order 'ASC'
-    
+    const sortOrder = req.query.sortOrder || 'ASC'; // Default sort order 'ASC'
+
     let orderByClause;
     if (sortBy === 'checked') {
-        orderByClause = 'checked ASC, item ASC'; // Sort by 'checked' then 'item'
+        orderByClause = 'checked DESC, item ASC'; // Sort by 'checked' then 'item'
     } else {
         orderByClause = 'item ASC, checked DESC'; // Default: sort by 'item' then 'checked'
     }
@@ -49,7 +57,6 @@ app.post('/add', (req, res) => {
     const { item } = req.body;
     db.query('INSERT INTO items (item, checked) VALUES (?, false)', [item], (err, result) => {
         if (err) throw err;
-        console.log('Item adicionado:', [item]);
         io.emit('item-added', { id: result.insertId, item, checked: false });
         res.redirect('/');
     });
@@ -57,9 +64,10 @@ app.post('/add', (req, res) => {
 
 app.post('/check', (req, res) => {
     const { id, checked } = req.body;
-    db.query('UPDATE items SET checked = ? WHERE id = ?', [checked === 'on' ? 1 : 0, id], (err, result) => {
+    const isChecked = checked === 'on' ? 1 : 0;
+    db.query('UPDATE items SET checked = ? WHERE id = ?', [isChecked, id], (err, result) => {
         if (err) throw err;
-        io.emit('item-checked', { id, checked: checked === 'on' ? 1 : 0 });
+        io.emit('item-checked', { id, checked: isChecked });
         res.redirect('/');
     });
 });
